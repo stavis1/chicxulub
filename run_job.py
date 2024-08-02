286856087f1dba53f47c361dcc75f3b15c45faa8
Steven Tavis <stavis@vols.utk.edu>
2024-08-02 17:13:22 -0400

    temporary hack to allow all raw files to be searched against a signle .faa

--- run_job.py
+++ run_job.py
@@ -14,8 +14,11 @@ faa_files = [f for f in os.listdir() if f.endswith('.faa')]
 identifiers = [re.search(r'\A([^_]+)_', f).group(1) for f in faa_files]
 basenames = [re.search(r'(.+)\.[^\.]+\Z',f).group(1) for f in os.listdir() if f.lower().endswith('.raw')]
 
-jobs = [(faa, next((b for b in basenames if i.lower() in b.lower()), None)) for i,faa in zip(identifiers, faa_files)]
-jobs = [j for j in jobs if j[1] is not None]
+if len(faa_files) > 1:
+    jobs = [(faa, next((b for b in basenames if i.lower() in b.lower()), None)) for i,faa in zip(identifiers, faa_files)]
+    jobs = [j for j in jobs if j[1] is not None]
+else:
+    jobs = [(faa_files[0], b) for b in basenames]
 
 with open('faa_list.txt', 'w') as faa_out:
     faa_out.write('\n'.join(j[0] for j in jobs))
