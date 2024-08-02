6479b1c442bb95f44dc3e2466895c641f8f4ad5d
Steven L Tavis <stavis@vols.utk.edu>
2024-08-02 16:38:49 -0400

    bugfixes in identifier matching and array submission code

--- run_job.py
+++ run_job.py
@@ -14,13 +14,13 @@ faa_files = [f for f in os.listdir() if f.endswith('.faa')]
 identifiers = [re.search(r'\A([^_]+)_', f).group(1) for f in faa_files]
 basenames = [re.search(r'(.+)\.[^\.]+\Z',f).group(1) for f in os.listdir() if f.lower().endswith('.raw')]
 
-jobs = [(faa, next((b for b in basenames if i in b), None)) for i,faa in zip(identifiers, faa_files)]
+jobs = [(faa, next((b for b in basenames if i.lower() in b.lower()), None)) for i,faa in zip(identifiers, faa_files)]
 jobs = [j for j in jobs if j[1] is not None]
 
 with open('faa_list.txt', 'w') as faa_out:
     faa_out.write('\n'.join(j[0] for j in jobs))
 with open('basename_list.txt', 'w') as bnames_out:
     bnames_out.write('\n'.join(j[0] for j in jobs))
-    
-subprocess.run(f'sbatch --array1-{len(jobs)} run_pipeline.sbatch', shell = True)
+
+subprocess.run(f'sbatch --array=1-{len(jobs)} run_pipeline.sbatch', shell = True)
 
