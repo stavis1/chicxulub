3b1097c0c46d245416c6052ed431c09543adab9c
Steven L Tavis <stavis@vols.utk.edu>
2024-08-02 14:49:47 -0400

    bugfix: added spaces in bash condition checks

--- run_batch_job.sh
+++ run_batch_job.sh
@@ -15,28 +15,28 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ ! -d $SCRIPT_DIR/env/search_env]; then
+if [ ! -d $SCRIPT_DIR/env/search_env ]; then
 	conda env create -p $SCRIPT_DIR/env/search_env -f $SCRIPT_DIR/env/search_env.yml
 fi
 
 cd $SCRIPT_DIR/exes/
-if [ ! -f comet.linux.exe]; then
+if [ ! -f comet.linux.exe ]; then
         wget https://github.com/UWPR/Comet/releases/download/v2024.01.1/comet.linux.exe
 fi
 
-if [ ! -f Dinosaur-1.2.0.free.jar]; then
+if [ ! -f Dinosaur-1.2.0.free.jar ]; then
         wget https://github.com/fickludd/dinosaur/releases/download/1.2.0/Dinosaur-1.2.0.free.jar
 fi
 
-if [ ! -f msconvert.sif]; then
+if [ ! -f msconvert.sif ]; then
 	singularity build --fakeroot msconvert.sif docker://proteowizard/pwiz-skyline-i-agree-to-the-vendor-licenses:latest
 fi
 
-if [ ! -f flashlfq.sif]; then
+if [ ! -f flashlfq.sif ]; then
         singularity build --fakeroot flashlfq.sif docker://smithchemwisc/flashlfq:latest
 fi
 
-if [ ! -f percolator.sif]; then
+if [ ! -f percolator.sif ]; then
 	wget https://github.com/percolator/percolator/releases/download/rel-3-07-01/percolator-converters-v3-07-linux-amd64.deb
         singularity build --fakeroot percolator.sif percolator.def
 fi
