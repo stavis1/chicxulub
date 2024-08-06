9bd8ecfe411c6c4d171acebf125f5064f07fb1c0
Steven L Tavis <stavis@vols.utk.edu>
2024-08-06 10:51:55 -0400

    now also removing asterisks from sequences

--- clean_fasta.py
+++ clean_fasta.py
@@ -5,4 +5,5 @@ with open(sys.argv[1], 'r') as faa_in:
     with open(f'filtered_{sys.argv[1]}', 'w') as faa_out:
         for line in faa_in:
             line = re.sub(r'[^ -~\n]', '', line)
+            line = re.sub(r'\*', '', line)
             faa_out.write(line)
