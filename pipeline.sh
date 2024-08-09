e50c0d7e456adf3c1a700e93c7ba649796111c61
Steven Tavis <stavis@vols.utk.edu>
2024-08-09 12:32:36 -0400

    removed dinosaur from pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 faa=$(sed "${1}q;d" faa_list.txt)
 name=$(sed "${1}q;d" basename_list.txt)
-echo $1
-echo $faa
-echo $name
+echo $1 | tee /dev/stderr
+echo $faa | tee /dev/stderr
+echo $name | tee /dev/stderr
 
 singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash msconvert.sif wine msconvert --outdir /data/ --outfile $name.indexed.mzML /data/$name.raw
 conda run -n search_env python clean_fasta.py $faa
 ./comet.linux.exe -Pcomet.params -Dfiltered_$faa $name.indexed.mzML
 grep -v -- 'nan' $name.indexed.pin > $name.filtered.pin
-singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash percolator.sif percolator -U --reset-algorithm -m /data/$name.pout /data/$name.filtered.pin
-conda run -n search_env python percolator_to_flashlfq.py $name.pout
+singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash percolator.sif percolator -U --reset-algorithm -w /data/$name.weights -m /data/$name.pout /data/$name.filtered.pin
 mkdir $name.results
 cp $name.pout $name.results
-java -jar Dinosaur-1.2.0.free.jar --outDir=$name.results $name.indexed.mzML
-conda run -n search_env python match_dinosaur_peaks.py -d $name.results/$name.indexed.features.tsv -p $name.txt -o $name.results/$name.peptide_AUC.tsv
 cp -r $name.results ../
