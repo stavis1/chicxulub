6226e2e716425dd3d259d531302675dc937b20c9
Steven Tavis <stavis@vols.utk.edu>
2024-08-13 10:35:40 -0400

    bugfix: now writiting a filtered faa per file to avoid race conditions

--- clean_fasta.py
+++ clean_fasta.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 with open(sys.argv[1], 'r') as faa_in:
-    with open(f'filtered_{sys.argv[1]}', 'w') as faa_out:
+    with open(sys.argv[2], 'w') as faa_out:
         for line in faa_in:
             line = re.sub(r'[^ -~\n]', '', line)
             line = re.sub(r'\*', '', line)
--- pipeline.sh
+++ pipeline.sh
@@ -6,8 +6,8 @@ echo $faa | tee /dev/stderr
 echo $name | tee /dev/stderr
 
 singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash msconvert.sif wine msconvert --outdir /data/ --outfile $name.indexed.mzML /data/$name.raw
-conda run -n search_env python clean_fasta.py $faa
-./comet.linux.exe -Pcomet.params -Dfiltered_$faa $name.indexed.mzML
+conda run -n search_env python clean_fasta.py $faa $name.faa
+./comet.linux.exe -Pcomet.params -D$name.faa $name.indexed.mzML
 grep -v -- 'nan' $name.indexed.pin > $name.filtered.pin
 singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash percolator.sif percolator -U --reset-algorithm -w /data/$name.weights -m /data/$name.pout /data/$name.filtered.pin
 mkdir $name.results
