93d383fb09307abd38e2bc2140346fe60e38138a
Steven Tavis <stavis@vols.utk.edu>
2024-08-14 15:42:01 -0400

    downgraded percolator version due to several bugs with the newest release

--- pipeline.sh
+++ pipeline.sh
@@ -9,7 +9,7 @@ singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash msconvert
 conda run -n search_env python clean_fasta.py $faa $name.faa
 ./comet.linux.exe -Pcomet.params -D$name.faa $name.indexed.mzML
 grep -v -- 'nan' $name.indexed.pin > $name.filtered.pin
-singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash percolator.sif percolator -U --reset-algorithm -w /data/$name.weights -m /data/$name.pout /data/$name.filtered.pin
+singularity run --fakeroot --containall --bind ./:/data/ -w --unsquash percolator.sif percolator -U --reset-algorithm --weights /data/$name.weights -m /data/$name.pout /data/$name.filtered.pin
 mkdir $name.results
 cp $name.pout $name.results
 cp -r $name.results ../
