43b72121d3148b1172357f73aa0e05da3c510890
Steven Tavis <stavis@vols.utk.edu>
2024-09-26 09:30:35 -0400

    wrapped msconvert call in bash script to work around nextflow issue

---
+++ msconvert.sh
@@ -0,0 +1,8 @@
+launchDir=$1
+msconvert=$2
+spectra=$3
+wine_temp=$(mktemp -d)
+
+singularity run --containall --bind $launchDir:/data/ --bind $wine_temp:/wineprefix64 $msconvert bash /run_msconvert.sh "--outdir /data/ --outfile $spectra.mzML /data/$spectra"
+rm -rf $wine_temp
+mv $launchDir/$spectra.mzML ./
