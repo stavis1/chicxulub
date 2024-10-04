bbf8cf484948d6512d93ed31b84199940616ff47
Steven Tavis <stavis@vols.utk.edu>
2024-10-04 13:15:17 -0400

    updated docker build resources

---
+++ exes/build_all.sh
@@ -0,0 +1,17 @@
+docker build -t stavisvols/params_parser -f params_parser.dockerfile ./
+docker push stavisvols/params_parser
+
+docker build -t stavisvols/msconvert -f msconvert.dockerfile ./
+docker push stavisvols/msconvert
+
+docker build -t stavisvols/comet_for_pipeline -f comet.dockerfile ./
+docker push stavisvols/comet_for_pipeline
+
+docker build -t stavisvols/percolator_for_pipeline -f percolator.dockerfile ./
+docker push stavisvols/percolator_for_pipeline
+
+docker build -t stavisvols/dinosaur_for_pipeline -f dinosaur.dockerfile ./
+docker push stavisvols/dinosaur_for_pipeline
+
+docker build -t stavisvols/feature_mapper -f feature_mapper.dockerfile ./
+docker push stavisvols/feature_mapper
---
+++ exes/run_msconvert.sh
@@ -0,0 +1,2 @@
+cp -r /temporary_wine_dir/* /wineprefix64 
+wine msconvert $1
