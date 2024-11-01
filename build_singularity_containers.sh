5e9913d3d1bff999c556d53f4bbb6f14428f2bb1
Steven Tavis <stavis@vols.utk.edu>
2024-11-01 17:41:02 -0400

    bugfix: properly specifying loop

--- build_singularity_containers.sh
+++ build_singularity_containers.sh
@@ -2,7 +2,7 @@
 containers=("params_parser" "comet_for_pipeline" "percolator_for_pipeline" "dinosaur_for_pipeline"
  "feature_mapper" "eggnog_for_pipeline" "quantify_annotations" "msconvert")
 
-for container in $containers
+for container in "${containers[@]}"
 do
     singularity build cache/stavisvols-$container-latest.img docker://stavisvols/$container:latest
 done
