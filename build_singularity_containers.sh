4bc47b938410d0e58818ffc1a4731d5bb2dad20f
Steven Tavis <stavis@vols.utk.edu>
2024-11-01 17:28:59 -0400

    added build script for singularity containers

---
+++ build_singularity_containers.sh
@@ -0,0 +1,7 @@
+containers=("params_parser" "comet_for_pipeline" "percolator_for_pipeline" "dinosaur_for_pipeline"
+ "feature_mapper" "eggnog_for_pipeline" "quantify_annotations" "msconvert")
+
+for container in $containers
+do
+    singularty build cache/stavisvols-$container-latest.img docker://stavisvols/container:latest
+done
