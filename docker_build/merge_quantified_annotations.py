a331f3e7c291e7a989eb258aee5b0c8939b98627
Steven Tavis <stavis@vols.utk.edu>
2025-01-13 10:18:45 -0500

    bugfix: now properly parsing peptide intensity file names

--- docker_build/merge_quantified_annotations.py
+++ docker_build/merge_quantified_annotations.py
@@ -58,7 +58,7 @@ for f in pep_files:
     newdata = pd.read_csv(f, sep = '\t')
     newdata.index = newdata['sequence']
     del newdata['sequence']
-    suffix = re.search(f'([^\\\\/]+)\\.intensities\\..+\\.{args.extension}\\Z', f).group(1)
+    suffix = re.search(f'([^\\\\/]+)\\.intensities\\Z', f).group(1)
     newdata.columns = [f'{c}_{suffix}' for c in newdata.columns]
     data.append(newdata)
 data = pd.concat(data, axis = 1)
